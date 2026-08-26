fb8edd7dfa5706996a5926c5016aaa35a0540791
etlasstudio8-dot <etlasstudio8@email.com>
2026-08-26 22:55:24 +0530

    Point frontend to Render API

--- cirvio-frontend/config.js
+++ cirvio-frontend/config.js
@@ -1,2 +1,2 @@
-window.CIRVIO_API_BASE = window.CIRVIO_API_BASE || 'http://localhost:5000';
+window.CIRVIO_API_BASE = window.CIRVIO_API_BASE || 'https://cirvio.onrender.com';
 window.CIRVIO_GOOGLE_CLIENT_ID = window.CIRVIO_GOOGLE_CLIENT_ID || '';
